a3cbc261dc1b09e1e5649c09a1399a26200ca303
zajnemzaj <dezsoj@gmail.com>
2021-12-17 10:10:09 +0100

    2-way data binding example

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,40 +1,53 @@
 <template>
-  <form>
+<form>
     <label>Email:</label>
-    <input type="email" required />
-  </form>
+    <input type="email" required v-model="email">
+
+    <label>Passworld:</label>
+    <input type="password" required v-model="password">
+</form>
+<p>Email: {{ email }}</p>
+<p>Password: {{ password }}</p>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    }
 }
 </script>
 
 <style>
 form {
-  max-width: 420px;
-  margin: 30px auto;
-  background: white;
-  text-align: left;
-  padding: 40px;
-  border-radius: 10px;
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
 }
+
 label {
-  color: #aaa;
-  display: inline-block;
-  margin: 25px 0 15px;
-  font-size: 0.6em;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  font-weight: bold;
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
 }
+
 input {
-  display: block;
-  padding: 10px 6px;
-  width: 100%;
-  box-sizing: border-box;
-  border: none;
-  border-bottom: 1px solid #ddd;
-  color: #555;
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
 }
 </style>

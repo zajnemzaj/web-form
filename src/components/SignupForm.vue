4be95f90b4d4cb854efb56a745664781953215da
zajnemzaj <dezsoj@gmail.com>
2021-12-17 19:18:22 +0100

    checking if password is at least 5 char and outputting error message below password input

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,6 +5,7 @@
 
     <label>Passworld:</label>
     <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -44,7 +45,8 @@ export default {
             role: 'designer',
             terms: false,
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: ''
         }
     },
     methods: {
@@ -62,7 +64,9 @@ export default {
             }) // filter: cycles through
         },
         handleSubmit() {
-            console.log('form submitted')
+            // validate Password
+            this.passwordError = this.password.length > 5 ?
+            '' : 'Password must be at least 6 chars long'
         }
     }
 }
@@ -132,5 +136,12 @@ button {
 
 .submit {
     text-align: center;
+}
+
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
